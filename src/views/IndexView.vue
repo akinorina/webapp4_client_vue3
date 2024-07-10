75d841b2cc3630350253dec5f3ef7be40ea3a636
Akinori Nakata <akinori.nakata@bright-century.com>
2024-07-10 13:11:10 +0900

    Sass 導入とコード simple化

---
+++ src/views/IndexView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="index">
+      Index
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+
+main {
+  width: 100vw;
+  text-align: center;
+  border: 1px red dashed;
+  font-size: 22rem;
+}
+
+</style>
